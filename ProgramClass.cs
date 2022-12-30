79cb46ab489e4c60f762147a8bbc7468657edac6
Bruno Morais <119684526+Brunomorais10@users.noreply.github.com>
2022-12-30 17:16:59 -0300

    Create ProgramClass.cs

---
+++ ProgramClass.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Programa3
+{
+    internal class Dados
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+
+    }
+}
